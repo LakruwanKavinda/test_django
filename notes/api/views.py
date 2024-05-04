460e02bf0a72fb8d741eca7c39c5e57705cfe936
LakruwanKavinda <wdmlkdissanayake@std.appsc.sab.ac.lk>
2024-05-04 11:23:43 +0530

    mobile_with_django_version2

---
+++ notes/api/views.py
@@ -0,0 +1,63 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of notes'
+        },
+        {
+            'Endpoint': '/notes/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single note object'
+        },
+        {
+            'Endpoint': '/notes/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new note with data sent in post request'
+        },
+        {
+            'Endpoint': '/notes/id/update/',
+            'method': 'PUT',
+            'body': {'body': ""},
+            'description': 'Creates an existing note with data sent in'
+        },
+        {
+            'Endpoint': '/notes/id/delete/',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Delete and existing note'
+        },
+    ]
+    return Response(routes)
+
+
+def signup(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        username = data.get('username')
+        password = data.get('password')
+        if username and password:
+            # Check if the username is already taken
+            if User.objects.filter(username=username).exists():
+                return JsonResponse({'error': 'Username already exists'}, status=400)
+            # Create the user
+            user = User.objects.create_user(
+                username=username, password=password)
+            # Authenticate the user
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                return JsonResponse({'token': 'your_auth_token'}, status=201)
+            else:
+                return JsonResponse({'error': 'Authentication failed'}, status=400)
+        else:
+            return JsonResponse({'error': 'Invalid request'}, status=400)
+    else:
+        return JsonResponse({'error': 'Method not allowed'}, status=405)
