bed087ea530a1909f19d4ddbe1e7e4b3762a0082
Lakruwan Kavinda <137679190+LakruwanKavinda@users.noreply.github.com>
2024-05-05 19:16:40 +0530

    editted_1_Update urls.py
    editted1

--- notes/api/urls.py
+++ notes/api/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.user_login, name='login'),
+    path('upload-pdf/', views.upload_pdf_view, name='upload_pdf'),
+    path('search/', views.search_pdf, name='search_pdf'),
+    path('pdf/<int:pdf_id>/', views.show_pdf, name='show_pdf'),
     path('', views.getRoutes),
 ]
