6310070880160d11814a3f77c3ebb42a5b1836cf
Lakruwan Kavinda <137679190+LakruwanKavinda@users.noreply.github.com>
2024-05-05 19:15:50 +0530

    editted_1_Update models.py
    editted1

--- notes/api/models.py
+++ notes/api/models.py
@@ -1,15 +1,17 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Note(models.Model):
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
-    def _str_(self):
+    def __str__(self):  # Corrected method name
         return self.body[0:50]
 
     class Meta:
         ordering = ['-updated']
+
+
+class PDFFile(models.Model):
+    # Your model fields and methods here
