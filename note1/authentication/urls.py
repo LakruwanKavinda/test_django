2213678b1b7ea9e4f4d68a87ece9ffefe0fb75cb
Lakruwan Kavinda <137679190+LakruwanKavinda@users.noreply.github.com>
2024-05-12 12:25:31 +0530

    Update urls2.py
    Update urls2.py

--- note1/authentication/urls.py
+++ note1/authentication/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('search_pdf/', views.search_pdf, name='search_pdf'),
     path('generate_story/', views.generate_story, name='generate_story'),
     path('view_pdf/<int:pdf_id>/', views.view_pdf, name='view_pdf'),
+    # chat
+    path('send_message/', views.send_message, name='send_message'),
+    path('get_messages/', views.get_messages, name='get_messages'),
 ]
