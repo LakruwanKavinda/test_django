b76900b5d93c270e758c229de34b46680ccda4f7
Lakruwan Kavinda <137679190+LakruwanKavinda@users.noreply.github.com>
2024-05-12 12:27:33 +0530

    Update models2.py
    Update models2.py

--- note1/authentication/models.py
+++ note1/authentication/models.py
@@ -12,3 +12,17 @@ class PDFFile(models.Model):
 
     def __str__(self):
         return self.name
+
+# chat
+
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.sender} -> {self.receiver}: {self.content}'
