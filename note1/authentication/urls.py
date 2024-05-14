f4c35a938433dc3829e1984c6c0124772f0ea4ca
Lakruwan Kavinda <137679190+LakruwanKavinda@users.noreply.github.com>
2024-05-14 16:06:09 +0530

    Update3 urls.py
    Update3 urls.py

--- note1/authentication/urls.py
+++ note1/authentication/urls.py
@@ -1,8 +1,13 @@
+"""
+URL configuration for authentication app.
+"""
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
+    # Change 'login' to 'user_login'
     path('login/', views.login, name='login'),
     path('upload_pdf/', views.upload_pdf, name='upload_pdf'),
     path('search_pdf/', views.search_pdf, name='search_pdf'),
